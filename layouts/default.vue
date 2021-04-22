babe8fec9831daf83fdcce8d199e2286e9af3d5c
Muhammad Faraz <muhammadfaraz991@gmail.com>
2021-04-23 02:11:52 +0500

    nuxt js basics

---
+++ layouts/default.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app dark>
+    
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>mdi-minus</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+     <n-link to="/">Home</n-link>
+     <n-link to="/weather-app">Weather app</n-link>
+     
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  
+    
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+<style>
+a{
+  text-decoration: none;
+}
+</style>
---
+++ pages/weather-app/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <!-- <v-layout> -->
+
+    <h1 class="display-1 text-xs-center">Weather App</h1>
+    <v-flex xs12>
+      <v-card dark>
+        <v-card-text>Hello world {{ this.name }}</v-card-text>
+      </v-card>
+    </v-flex>
+    <v-form @submit.prevent="getWeatherInfo">
+      <v-text-field label="Enter City name" v-model="city"></v-text-field>
+    </v-form>
+    <!-- </v-layout> -->
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      city: "London",
+      name:'',
+      result:{
+
+      }
+    };
+  },
+  created() {
+    
+  },
+  methods: {
+      getWeatherInfo() {
+this.$axios
+      .$get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${process.env.weatherAppId}
+`
+      )
+      .then((res) => {
+          this.result=res
+      });
+      }
+      
+  }
+};
+</script>
